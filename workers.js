5e1da31c2c066b5fbd56150dce9320ec39a1b76f
Akkariin Meiko <akkariins@gmail.com>
2019-06-10 00:28:14 +0800

    Update workers.js

--- workers.js
+++ workers.js
@@ -1,5 +1,29 @@
+/**
+ *
+ *  Cloudflare Workers Blog
+ *
+ *  https://github.com/kasuganosoras/cloudflare-worker-blog
+ *
+ */
+
 // 定义 Github 项目，文章会从这里读取
 const github_base = "kasuganosoras/cloudflare-worker-blog";
+
+// 站点名称
+const site_name = "CloudFlare Workers Blog";
+
+// 站点介绍
+const site_desc = "一个用 CloudFlare Worker 搭建的博客";
+
+// 资源文件
+const highlight_css = "https://cn.tql.ink:4443/css/highlight.js/github.css";
+const highlight_main = "https://cn.tql.ink:4443/js/highlight.min.js";
+const highlight_pack = "https://cn.tql.ink:4443/js/highlight.pack.js";
+
+// Copyright
+const copyright = `&copy; 2019 ${site_name} | <a href="https://github.com/kasuganosoras/cloudflare-worker-blog" target="_blank">Github</a>`;
+
+/* 配置到此结束 */
 
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
@@ -11,37 +35,28 @@ var header = `
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=11">
-		<meta name="application-name" content="CloudFlare Worker Blog">
+		<meta name="application-name" content="${site_name}">
 		<meta name="msapplication-TileColor" content="#F1F1F1">
-		<meta name="description" content="一个用 CloudFlare Worker 搭建的博客">
+		<meta name="description" content="${site_desc}">
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
-		<link rel="stylesheet" href="https://cn.tql.ink:4443/css/highlight.js/github.css">
-    <title>CloudFlare Workers Blog</title>
+		<link rel="stylesheet" href="${highlight_css}">
+		<title>${site_name}</title>
 		<script src="https://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/showdown/1.9.0/showdown.min.js" type="text/javascript"></script>
-    <script src="https://cdn.jsdelivr.net/npm/showdown-table@1.0.1/dist/showdown-table.min.js" type="text/javascript"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/showdown/1.9.0/showdown.min.js" type="text/javascript"></script>
+		<script src="https://cdn.jsdelivr.net/npm/showdown-table@1.0.1/dist/showdown-table.min.js" type="text/javascript"></script>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" crossorigin="anonymous"></script>
-		<script src="https://cn.tql.ink:4443/js/highlight.min.js"></script>
-		<script src="https://cn.tql.ink:4443/js/highlight.pack.js"></script>
-    <style type="text/css">.post-a {color: #000;text-decoration: none ! important;}.post-box {padding: 12px 20px 12px 20px;border-bottom: 1px solid rgba(0,0,0,0.07);cursor: pointer;border-left: 0px solid rgba(66, 66, 66, 0);transition-duration: 0.3s;}.post-box:hover {transition-duration: 0.3s;border-left: 5px solid rgba(66, 66, 66, 0.15);}.thread h2 {border-bottom: 1px solid rgb(238,238,238);padding-bottom: 10px;}.editor-preview pre, .editor-preview-side pre{padding: 0.5em;}.hljs{background: unset ! important;padding: 0px;}.CodeMirror{height: calc(100% - 320px);min-height: 360px;}.msgid{font-family:Consolas;}.tooltip {word-break: break-all;}h2 a{font-weight: 400;}body{font-family: '-apple-system','BlinkMacSystemFont','Segoe UI','Helvetica','Arial','sans-serif','Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol' ! important;font-weight: 400;}h2 a{color: #000;} h2 a:hover{color: #000; text-decoration: none;}.full-width{width: 100%;}.thread img{vertical-align:text-bottom ! important;max-width:100% ! important;margin-top:8px;margin-bottom:8px;}.thread table{display:block;width:100%;overflow:auto;margin-bottom:8px;}.thread table tr{background-color:#fff;border-top:1px solid #c6cbd1;}.thread table tr:nth-child(2n){background-color:#f6f8fa;}.thread table th,.thread table td{padding:6px 13px;border:1px solid #dfe2e5;font-size:14px;}.thread pre{margin-bottom:16px;}pre{border:none ! important;}blockquote{font-size:15px ! important;}</style>
-		<script>
-			var _hmt = _hmt || [];
-			(function() {
-			  var hm = document.createElement("script");
-			  hm.src = "https://hm.baidu.com/hm.js?0d0ea12028a06ba9e20deadc84430516";
-			  var s = document.getElementsByTagName("script")[0]; 
-			  s.parentNode.insertBefore(hm, s);
-			})();
-		</script>
+		<script src="${highlight_main}"></script>
+		<script src="${highlight_pack}"></script>
+		<style type="text/css">.post-a {color: #000;text-decoration: none ! important;}.post-box {padding: 12px 20px 12px 20px;border-bottom: 1px solid rgba(0,0,0,0.07);cursor: pointer;border-left: 0px solid rgba(66, 66, 66, 0);transition-duration: 0.3s;}.post-box:hover {transition-duration: 0.3s;border-left: 5px solid rgba(66, 66, 66, 0.15);}.thread h2 {border-bottom: 1px solid rgb(238,238,238);padding-bottom: 10px;}.editor-preview pre, .editor-preview-side pre{padding: 0.5em;}.hljs{background: unset ! important;padding: 0px;}.CodeMirror{height: calc(100% - 320px);min-height: 360px;}.msgid{font-family:Consolas;}.tooltip {word-break: break-all;}h2 a{font-weight: 400;}body{font-family: '-apple-system','BlinkMacSystemFont','Segoe UI','Helvetica','Arial','sans-serif','Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol' ! important;font-weight: 400;}h2 a{color: #000;} h2 a:hover{color: #000; text-decoration: none;}.full-width{width: 100%;}.thread img{vertical-align:text-bottom ! important;max-width:100% ! important;margin-top:8px;margin-bottom:8px;}.thread table{display:block;width:100%;overflow:auto;margin-bottom:8px;}.thread table tr{background-color:#fff;border-top:1px solid #c6cbd1;}.thread table tr:nth-child(2n){background-color:#f6f8fa;}.thread table th,.thread table td{padding:6px 13px;border:1px solid #dfe2e5;font-size:14px;}.thread pre{margin-bottom:16px;}pre{border:none ! important;}blockquote{font-size:15px ! important;}</style>
 	</head>
   <body>
     <div class="container">
       <div class="row">
         <div class="col-sm-2"></div>
         <div class="col-sm-8">
-          <h2><a href="/" class="post-a">Workers Blog</a></h2>
-          <p>一个基于 CloudFlare workers 开发的博客</p>
+          <h2><a href="/" class="post-a">${site_name}</a></h2>
+          <p>${site_desc}</p>
           <hr>
           <div class="thread">
 `;
@@ -96,7 +111,7 @@ async function bloghandle(request) {
     }
     data += `
             <br>
-            <p class="text-right">Powered by CloudFlare Workers | <a href="https://github.com/kasuganosoras/cloudflare-worker-blog" target="_blank">Github</a></p>
+            <p class="text-right">${copyright}</p>
           </div>
     `;
   } else {
@@ -123,7 +138,7 @@ async function bloghandle(request) {
             \`;
           </script>
           <hr>
-          <p class="text-right">Powered by CloudFlare Workers | <a href="https://github.com/kasuganosoras/cloudflare-worker-blog" target="_blank">Github</a></p>
+          <p class="text-right">${copyright}</p>
     `;
   }
   data += `
